0ff69c9e84b5b72e13de934c292b2f395f01c511
mickykate <ka12miyuki@gmail.com>
2024-05-14 21:34:21 +0900

    ”電卓課題”

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let state = "start";
-let mode = "integer_mode";
+'use strict'
 
+let result = document.getElementsByClassName('display')
 
 function calc_number(btn) {
-    if(btn.value == "=") {
+    if (btn.value == "=") {
       document.calculator.display.value = eval(document.calculator.display.value);
     } else if (btn.value == "AC") {
       document.calculator.display.value = "";
@@ -18,11 +18,10 @@ function calc_number(btn) {
       document.calculator.div_btn.value = "÷";
     }
   }
-  
-  
 
+//四則演算の連続入力制御・→ 修正中
 
-
+//ゼロの連続入力阻止。→ 後ほど修正
 
 
 
